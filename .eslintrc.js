c9b5271b40467d0334bca95a13a72029178acc49
Wv98-web <418060258@qq.com>
2023-03-14 17:33:35 +0800

    代码检查, 代码格式

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,16 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020
   },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   rules: {
+    // 禁止使用 var
+    'no-var': "error",
+    // 优先使用 interface 而不是 type
+    '@typescript-eslint/consistent-type-definitions': [
+      "error",
+      "interface"
+    ],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
